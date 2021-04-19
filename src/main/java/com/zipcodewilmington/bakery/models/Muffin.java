88e4335c8bbc0a8f9cfb338df426fb59f3726e38
MNinh <msninh2@gmail.com>
2021-04-18 22:59:21 -0400

    completed

--- src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
@@ -1,10 +1,16 @@
 package com.zipcodewilmington.bakery.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
-
+@Entity
 public class Muffin {
 
-    private Long id;
+
+    private @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
 
     private String flavor;
 
